d6d6df1b749aa3dbc711a8d74189675b6237fb91
Carlosromero779 <carlosromeros779@gmail.com>
2025-10-20 14:34:57 -0400

    first commit

---
+++ hola.py
@@ -0,0 +1,10 @@
+def saludar(mensaje):
+    print(mensaje)
+    
+saludar('Hola como te va ricardo')
+
+def suma(valor1, valor2):
+    resultado = valor1 + valor2
+    return resultado
+
+print(f'El resultado es el siguiente {suma(1,2)}')
